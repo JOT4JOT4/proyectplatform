5f50dc44c34e42cc22687d679536176185c2fb9b
JOT4JOT4 <you@example.com>
2026-07-04 12:30:22 -0400

    una linea de codigo

--- ECIN_UCN-ReserveProject/backend/src/auth/auth.controller.ts
+++ ECIN_UCN-ReserveProject/backend/src/auth/auth.controller.ts
@@ -41,7 +41,7 @@ export class AuthController {
   async googleWebAuthRedirect(@Req() req, @Res() res: Response) {
     const loginData = await this.authService.googleLogin(req);
     const code = this.authService.createLoginCode(loginData);
-    const frontendUrl = process.env.FRONTEND_URL || 'http://reservasucn.vercel.app';
+    const frontendUrl = process.env.FRONTEND_URL || 'http://reservasucn-production.up.railway.app';
 
     return res.redirect(
       `${frontendUrl}/auth/callback?code=${code}`,
