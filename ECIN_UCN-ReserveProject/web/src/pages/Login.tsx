d630ad4bf67c864d1fd77dcb8323a66374f0ca33
Tobal29 <cristobal.luna@alumnos.ucn.cl>
2026-07-04 22:07:09 -0400

    imagenes del login

--- ECIN_UCN-ReserveProject/web/src/pages/Login.tsx
+++ ECIN_UCN-ReserveProject/web/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { getApiUrl } from "../config";
+<<<<<<< Updated upstream
 import "../css/login.css";
 
 export default function Login() {
@@ -17,12 +18,34 @@ export default function Login() {
                 <div className="divider">
                     <span className="divider-text">Iniciar sesión</span>
                 </div>
+=======
+import logoUcn from "../../assets/logo-ucn.png";
+import googleLogo from "../../assets/google.png";
+import "../css/login.css";
 
-                <button className="google-btn" onClick={handleLogin}>
-                    <img src="../../assets/google.png" className="logo-google" />
-                    Iniciar sesión con Google
-                </button>
+export default function Login() {
+  const handleLogin = () => {
+    window.location.href = `${getApiUrl()}/auth/google/web`;
+  };
 
+  return (
+    <div className="login-container">
+      <img src={logoUcn} className="logo-ucn" alt="Logo Universidad Católica del Norte" />
+
+      <div className="login-box">
+        <h1 className="login-title">Login</h1>
+
+        <div className="divider">
+          <span className="divider-text">Iniciar sesión</span>
+        </div>
+>>>>>>> Stashed changes
+
+        <button className="google-btn" onClick={handleLogin}>
+          <img src={googleLogo} className="logo-google" alt="Logo Google" />
+          Iniciar sesión con Google
+        </button>
+
+<<<<<<< Updated upstream
                 <p className="login-warning">
                     * Acceso exclusivo para correos institucionales de la UCN.*
                 </p>
@@ -33,3 +56,12 @@ export default function Login() {
         </div>
     )
 }
+=======
+        <p className="login-warning">
+          * Acceso exclusivo para correos institucionales de la UCN.*
+        </p>
+      </div>
+    </div>
+  );
+}
+>>>>>>> Stashed changes
